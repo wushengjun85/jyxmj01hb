d402408565013138a164adcfe3ee986dedc1040b
vmuser <vmuser@Linux-host.(none)>
2016-07-27 15:45:26 +0800

    version01

--- dialog.cpp
+++ dialog.cpp
@@ -30,57 +30,38 @@ extern unsigned char flagLeft;  //左转
 extern unsigned char countLeft; //左转
 
 extern unsigned char flagBattery;//电瓶指示灯
-extern unsigned char countBattery;//电瓶指示灯
 
 extern unsigned char flagWidthlamp; //示宽灯
-extern unsigned char countWidthlamp;//示宽灯
 
 extern unsigned char  flagYG; //远光灯
-extern unsigned char  countYG;//远光灯
 
 extern unsigned char  flagSW; //水温
-extern unsigned char  countSW; //水温
 
 extern unsigned char  flagJG; //近光灯
-extern unsigned char  countJG; //近光灯
-
 
 extern unsigned char  flagJY; //机油
-extern unsigned char  countJY; //机油
 
 extern unsigned char  flagLCM; //粮仓满
-extern unsigned char  countLCM; //粮仓满
 
 extern unsigned char   flagFDJYR; //发动机预热
-extern unsigned char   countFDJYR; //发动机预热
 
 extern unsigned char   flagGL; //过滤
-extern unsigned char   countGL; //过滤
 
 extern unsigned char   flagYL; //油量
-extern unsigned char    countYL;//油量
 
 extern unsigned char  flagYeyayouwen;//液压油温
-extern unsigned char  countYeyayouwen; //液压油温
 
 extern unsigned char    flagECU;//ecu
-extern unsigned char    countECU;//ecu
 
 extern unsigned char    flagPark;//停车
-extern unsigned char    countPark; //停车
 
 extern unsigned char   flagFDJGZ; //发动机故障
-extern unsigned char   countFDJGZ; //发动机故障
 
 extern unsigned char   flagRight; //右转
-extern unsigned char    countRight;//右转
 
 
 extern unsigned char  flagyouxiangman;//油量满报警
 extern unsigned char  flagyouliangdi; //油量低报警
-
-
-
 
 
 
@@ -103,11 +84,6 @@ extern ushort out4;//输出4 4,
 extern ushort out3;//输出3 3,
 extern ushort out2;//输出2 2,
 extern ushort out1;//输出1 1,
-
-
-
-
-
 
 /********************************************************************************************************************/
 
@@ -175,7 +151,7 @@ void Dialog::paintEvent(QPaintEvent *event)
     /**************************************************************************************************************/
 
 
-    /************************************************************************************************/
+    /*************************************************************************************************************/
     //闪烁不同步问题解决办法。
     //利用消息队列 和 for 循环解决。
     //2016.7.25
@@ -294,295 +270,10 @@ void Dialog::paintEvent(QPaintEvent *event)
 
     #endif
 
-
-
-
-    /************************************************************************************************/
-
-
-#if 0
-    if(flagLeft) //左转灯闪烁
-    {
-        countLeft++;
-        if (countLeft>1)
-            countLeft = 0;
-        switch(countLeft)
-        {
-            case 1:pixLeft.load("./img2/27.png");//14.jpg
-            break;
-        }
-        paintLeft.drawPixmap(0,0,43,34,pixLeft);
-
-    }
-
-
-    //电瓶指示灯
-    QPainter paintBattery(this);
-    QPixmap pixBattery;
-
-
-    if(flagBattery) //闪烁
-    {
-        countBattery++;
-        if (countBattery>1)
-            countBattery = 0;
-        switch(countBattery)
-        {
-            case 1:pixBattery.load("./img2/21.png");//14.jpg
-            break;
-        }
-        paintBattery.drawPixmap(51,0,43,34,pixBattery);
-    }
-
-    //示宽灯
-    QPainter paintWidthlamp(this);
-    QPixmap pixWidthlamp;
-    if(flagWidthlamp) //闪烁
-    {
-        countWidthlamp++;
-        if (countWidthlamp>1)
-            countWidthlamp = 0;
-        switch(countWidthlamp)
-        {
-            case 1:pixWidthlamp.load("./img2/25.png");//14.jpg
-            break;
-        }
-        paintWidthlamp.drawPixmap(102,0,43,34,pixWidthlamp);
-    }
-
-    //远光灯
-    QPainter paintYG(this);
-    QPixmap pixYG;
-    if(flagYG) //闪烁
-    {
-        countYG++;
-        if (countYG>1)
-            countYG = 0;
-        switch(countYG)
-        {
-            case 1:pixYG.load("./img2/24.png");//14.jpg
-            break;
-        }
-        paintYG.drawPixmap(153,0,43,34,pixYG);
-    }
-
-    //水温
-    QPainter paintSW(this);
-    QPixmap pixSW;
-    if(flagSW) //闪烁
-    {
-        countSW++;
-        if (countSW>1)
-            countSW = 0;
-        switch(countSW)
-        {
-            case 1:pixSW.load("./img2/12.png");//14.jpg
-            break;
-        }
-        paintSW.drawPixmap(204,0,35,35,pixSW);
-    }
-
-    //近光灯flagJG
-
-    QPainter paintJG(this);
-    QPixmap pixJG;
-    if(flagJG) //闪烁
-    {
-        countJG++;
-        if (countJG>1)
-            countJG = 0;
-        switch(countJG)
-        {
-            case 1:pixJG.load("./img2/15.png");//14.jpg
-            break;
-        }
-        paintJG.drawPixmap(255,0,38,41,pixJG);
-    }
-
-    //机油
-    QPainter paintJY(this);
-    QPixmap pixJY;
-    if(flagJY) //闪烁
-    {
-        countJY++;
-        if (countJY>1)
-            countJY = 0;
-        switch(countJY)
-        {
-            case 1:pixJY.load("./img2/22.png");//14.jpg
-            break;
-        }
-        paintJY.drawPixmap(306,0,43,43,pixJY);
-    }
-
-    //flagLCM = 1; //粮仓满
-    QPainter paintLCM(this);
-    QPixmap pixLCM;
-    if(flagLCM) //闪烁
-    {
-        countLCM++;
-        if (countLCM>1)
-            countLCM = 0;
-        switch(countLCM)
-        {
-            case 1:pixLCM.load("./img2/7.png");//14.jpg
-            break;
-        }
-        paintLCM.drawPixmap(357,0,43,43,pixLCM);
-    }
-
-    //flagFDJYR = 1; //发动机预热
-    QPainter paintFDJYR(this);
-    QPixmap pixFDJYR;
-    if(flagFDJYR) //闪烁
-    {
-        countFDJYR++;
-        if (countFDJYR>1)
-            countFDJYR = 0;
-        switch(countFDJYR)
-        {
-            case 1:pixFDJYR.load("./img2/17.png");//14.jpg
-            break;
-        }
-        paintFDJYR.drawPixmap(357,0,43,43,pixFDJYR);
-    }
-
-    // flagGL = 1; //过滤
-    QPainter paintGL(this);
-    QPixmap pixGL;
-    if(flagGL)
-    {
-        countGL++;
-        if (countGL>1)
-            countGL = 0;
-        switch(countGL)
-        {
-            case 1:pixGL.load("./img2/20.png");//14.jpg
-            break;
-        }
-        paintGL.drawPixmap(408,0,43,43,pixGL);
-    }
-
-    //flagYL = 1; //油量
-    QPainter paintYL(this);
-    QPixmap pixYL;
-    if(flagyouxiangman|flagyouliangdi)
-    {
-        countYL++;
-        if (countYL>1)
-            countYL = 0;
-        switch(countYL)
-        {
-            case 1:pixYL.load("./img2/23.png");//14.jpg
-            break;
-        }
-        paintYL.drawPixmap(459,0,43,43,pixYL);
-
-    }
-
-
-    //以上比例都是 + 51
-
-    //flagYeyayouwen = 1;//液压油温
-    QPainter paintYeyayouwen(this);
-    QPixmap pixYeyayouwen;
-    if(flagYeyayouwen)
-    {
-        countYeyayouwen++;
-        if (countYeyayouwen>1)
-            countYeyayouwen = 0;
-        switch(countYeyayouwen)
-        {
-            case 1:pixYeyayouwen.load("./img2/16.png");//14.jpg
-            break;
-        }
-        paintYeyayouwen.drawPixmap(523,0,43,43,pixYeyayouwen);//上边图标
-
-        //paintYeyayouwen.drawPixmap(20,230,43,43,pixYeyayouwen);//左边图标
-    }
-
-
-    //ecu
-        QPainter paintECU(this);
-        QPixmap pixECU;
-        if(flagECU)
-        {
-            countECU++;
-            if (countECU>1)
-                countECU = 0;
-            switch(countECU)
-            {
-                case 1:pixECU.load("./img2/91.png");//14.jpg
-                break;
-            }
-            paintECU.drawPixmap(585,0,43,32,pixECU);
-        }
-
-
-        //flagPark = 1;//停车
-        QPainter paintPark(this);
-        QPixmap pixPark;
-        if(flagPark)
-        {
-            countPark++;
-            if (countPark>1)
-                countPark = 0;
-            switch(countPark)
-            {
-                case 1:pixPark.load("./img2/18.png");//14.jpg
-                break;
-            }
-            paintPark.drawPixmap(640,0,43,34,pixPark);
-        }
-
-        //
-        //flagFDJGZ = 1; //发动机故障
-        QPainter paintFDJGZ(this);
-        QPixmap pixFDJGZ;
-        if(flagFDJGZ)
-        {
-            countFDJGZ++;
-            if (countFDJGZ>1)
-                countFDJGZ = 0;
-            switch(countFDJGZ)
-            {
-                case 1:pixFDJGZ.load("./img2/8.png");//14.jpg
-                break;
-            }
-            paintFDJGZ.drawPixmap(697,0,43,37,pixFDJGZ);
-        }
-
-
-
-        //flagRight = 1; //右转
-        QPainter paintRight(this);
-        QPixmap pixRight;
-        if(flagRight)
-        {
-            countRight++;
-            if (countRight>1)
-                countRight = 0;
-            switch(countRight)
-            {
-                case 1:pixRight.load("./img2/right.png");//14.jpg
-                break;
-            }
-            paintRight.drawPixmap(752,0,48,48,pixRight);
-        }
-
-
-
-#endif
-
     /**********************************************************************************************************************/
 
 
-
-
-
-
-
-    /********************************************************************************************************/
+    /**********************************************************************************************************************/
     //2016.7.6 radiobutton input and output;
 
     if (kg1)
